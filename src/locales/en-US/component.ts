38aa8b44d75352d9a716b85c4b22f8fcb8f45d51
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-01-25 00:26:34 +0900

    ui(SKUB-21): update ui on p.o.template page

--- src/locales/en-US/component.ts
+++ src/locales/en-US/component.ts
@@ -2,4 +2,11 @@ export default {
   'component.tagSelect.expand': 'Expand',
   'component.tagSelect.collapse': 'Collapse',
   'component.tagSelect.all': 'All',
+  'component.button.new-template': 'New Template',
+  'component.button.edit': 'Edit',
+  'component.button.copy': 'Copy',
+  'component.button.delete': 'Delete',
+  'component.table.column.name': 'Name',
+  'component.table.column.created': 'Created',
+  'component.table.column.modified': 'Modified',
 };
--- src/pages/Management/Settings/POTemplates/index.tsx
+++ src/pages/Management/Settings/POTemplates/index.tsx
@@ -1,92 +1,89 @@
-import { OTable } from "@/components/Globals/OTable"
-import { Button } from "antd"
+import { FormattedMessage } from '@umijs/max';
+import { Button, Card, Space, Table } from 'antd';
 
-export default function() {
+const potemplateList = [
+  {
+    id: 1,
+    name: 'Default Template',
+    status: 1,
+    created: '05/15/2015',
+    modified: '05/15/2017',
+  },
+  {
+    id: 2,
+    name: 'Sandi Test Received PO Quantity',
+    status: 1,
+    created: '06/08/2017',
+    modified: '03/03/2020',
+  },
+  {
+    id: 1,
+    name: "Sam's New Po Template",
+    status: 1,
+    created: '06/25/2017',
+    modified: '09/11/2019',
+  },
+  {
+    id: 1,
+    name: 'Mecca Sample',
+    status: 1,
+    created: '08/23/2017',
+    modified: '09/10/2020',
+  },
+  {
+    id: 1,
+    name: 'Default Template',
+    status: 1,
+    created: '08/23/2017',
+    modified: '08/23/2017',
+  },
+  {
+    id: 1,
+    name: 'Default PO Template',
+    status: 1,
+    created: '01/07/2018',
+    modified: '01/07/2018',
+  },
+];
 
-  const potemplateList = [
-    {
-      "id": 1,
-      "name": "Default Template",
-      "status": 1,
-      "created": "05/15/2015",
-      "modified": "05/15/2017"
-    },
-    {
-      "id": 2,
-      "name": "Sandi Test Received PO Quantity",
-      "status": 1,
-      "created": "06/08/2017",
-      "modified": "03/03/2020"
-    },
-    {
-      "id": 1,
-      "name": "Sam's New Po Template",
-      "status": 1,
-      "created": "06/25/2017",
-      "modified": "09/11/2019"
-    },
-    {
-      "id": 1,
-      "name": "Mecca Sample",
-      "status": 1,
-      "created": "08/23/2017",
-      "modified": "09/10/2020"
-    },
-    {
-      "id": 1,
-      "name": "Default Template",
-      "status": 1,
-      "created": "08/23/2017",
-      "modified": "08/23/2017"
-    },
-    {
-      "id": 1,
-      "name": "Default PO Template",
-      "status": 1,
-      "created": "01/07/2018",
-      "modified": "01/07/2018"
-    }
-  ];
+const TColumns = [
+  {
+    title: <FormattedMessage id="component.table.column.name" />,
+    dataIndex: 'name',
+    key: 'name',
+    width: '80%',
+  },
+  {
+    title: <FormattedMessage id="component.table.column.created" />,
+    dataIndex: 'created',
+    key: 'created',
+  },
+  {
+    title: <FormattedMessage id="component.table.column.modified" />,
+    dataIndex: 'modified',
+    key: 'modified',
+  },
+];
 
+export default function () {
   return (
-    <>
-      <div className="w-full flex flex-column h-screen">
-        <div className="horizon-content">
-          <Button>
-            New Template
-          </Button>
-          <Button>
-            Edit
-          </Button>
-          <Button>
-            Copy
-          </Button>
-          <Button>
-            Delete
-          </Button>
+    <Card style={{ width: '100%' }}>
+      <Space size={5}>
+        <Button>
+          <FormattedMessage id="component.button.new-template" />
+        </Button>
+        <Button>
+          <FormattedMessage id="component.button.edit" />
+        </Button>
+        <Button>
+          <FormattedMessage id="component.button.copy" />
+        </Button>
+        <Button>
+          <FormattedMessage id="component.button.delete" />
+        </Button>
+      </Space>
 
-          <OTable
-            columns={[
-              {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-              },
-              {
-                title: 'Created',
-                dataIndex: 'created',
-                key: 'created',
-              },
-              {
-                title: 'Modified',
-                dataIndex: 'modified',
-                key: 'modified',
-              },
-            ]} 
-            rows={potemplateList}>
-          </OTable>
-        </div>
-      </div>
-    </>
-  )
+      <Table columns={TColumns} dataSource={potemplateList} style={{ marginTop: 10 }} />
+    </Card>
+  );
 }
