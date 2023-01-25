b3d110d7face004c48391d653cb9cfc1cec6e94f
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-01-25 14:31:23 +0900

    fix user administration page buttons

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,51 @@
+import { renderSearchQuery } from '@/utils/common';
 import httpClient from '@/utils/http-client';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 
 export default () => {
-  const [user, setUser] = useState();
+  const [userList, setUserList] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [showInactive, setShowInactive] = useState(true);
 
-  const login = useCallback(async (credentials: any) => {
-    try {
-      const response = await httpClient.post('/auth/login', credentials);
-      setUser(response.data);
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+  const getUsers = useCallback((_query) => {
+    const query = renderSearchQuery(_query);
+    httpClient
+      .get('/api/users' + query)
+      .then((response) => setUserList(response.data))
+      .catch((err) => console.log(err));
   }, []);
 
-  return { user, login };
+  const createUser = (data) => {
+    return httpClient.post('/api/users', data).then((response) => {
+      if (showInactive === response.data.is_active) setUserList((prev) => [...prev, response.data]);
+    });
+  };
+
+  const updateUser = (_user) => {
+    return httpClient.put('/api/users/' + _user.id, _user).then((response) => {
+      if (showInactive === !!response.data.is_active) {
+        setUserList(
+          userList.map((_item) => (_item.id === response.data.id ? response.data : _item)),
+        );
+      } else {
+        setUserList(userList.filter((_item) => _item.id !== response.data.id));
+      }
+      setSelectedUser(null);
+    });
+  };
+
+  useEffect(() => {
+    getUsers({ is_active: showInactive });
+  }, [getUsers, showInactive]);
+
+  return {
+    userList,
+    selectedUser,
+    showInactive,
+    setSelectedUser,
+    setShowInactive,
+    getUsers,
+    createUser,
+    updateUser,
+  };
 };
