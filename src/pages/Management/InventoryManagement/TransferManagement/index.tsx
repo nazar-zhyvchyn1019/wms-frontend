5e7570d7aff58d37056b8dc0fc2779da50fb70a8
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-01-25 20:47:28 +0900

    ui(SKUB-26): build ui on transter tab in inventory page

--- src/pages/Management/InventoryManagement/TransferManagement/index.tsx
+++ src/pages/Management/InventoryManagement/TransferManagement/index.tsx
@@ -1,8 +1,139 @@
-import { Button, Input, Drawer, Card, Row, Col } from 'antd';
+import { Button, Input, Drawer, Card, Row, Col, Select, Checkbox, Table } from 'antd';
 import React, { useState } from 'react';
 import { data, historyData, recieveData } from './components/structure';
 import { OTable } from '@/components/Globals/OTable';
+import { CheckCircleFilled, EditTwoTone, LikeFilled, PlayCircleFilled } from '@ant-design/icons';
 const { Search } = Input;
+
+const Tcolumns = [
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status) =>
+      status === 'active' ? (
+        <CheckCircleFilled style={{ color: '#00FF00', fontSize: 15 }} />
+      ) : (
+        <PlayCircleFilled style={{ color: '#1292C6', fontSize: 15 }} />
+      ),
+    align: 'center',
+  },
+  {
+    title: 'Source Warehouse',
+    dataIndex: 'source_warehouse',
+    key: 'source_warehouse',
+  },
+  {
+    title: 'Order Number',
+    dataIndex: 'order_number',
+    key: 'order_number',
+  },
+  {
+    title: 'FBA Shipment',
+    dataIndex: 'fba_shipment',
+    key: 'fba_shipment',
+  },
+  {
+    title: 'Create Date',
+    dataIndex: 'created_date',
+    key: 'created_date',
+  },
+  {
+    title: 'Closed Date',
+    dataIndex: 'closed_date',
+    key: 'closed_date',
+  },
+  {
+    title: 'Notes',
+    dataIndex: 'notes',
+    key: 'notes',
+    render: () => <EditTwoTone />,
+    align: 'center',
+  },
+  {
+    title: 'Sales Channel',
+    dataIndex: 'sales_channel',
+    key: 'sales_channel',
+  },
+  {
+    title: 'Receiving Warehouse',
+    dataIndex: 'receiving_warehouse',
+    key: 'receiving_warehouse',
+    align: 'center',
+  },
+  {
+    title: 'Total',
+    dataIndex: 'total',
+    key: 'total',
+  },
+  {
+    title: 'Shipping',
+    dataIndex: 'shipping',
+    key: 'shipping',
+  },
+  {
+    title: 'Tracking',
+    dataIndex: 'tracking',
+    key: 'tracking',
+    render: (text) => <a>{text}</a>,
+  },
+];
+
+const Hcolumns = [
+  {
+    title: 'Edit Time',
+    dataIndex: 'edit_time',
+    key: 'edit_time',
+  },
+  {
+    title: 'User',
+    dataIndex: 'user',
+    key: 'user',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    key: 'description',
+  },
+];
+
+const Rcolumns = [
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status) =>
+      status === 'Complete' ? (
+        <LikeFilled style={{ color: 'red', fontSize: 15 }} />
+      ) : (
+        <PlayCircleFilled style={{ color: '#00FF00', fontSize: 15 }} />
+      ),
+    align: 'center',
+  },
+  {
+    title: 'Master SKU',
+    dataIndex: 'master_sku',
+    key: 'master_sku',
+  },
+  {
+    title: 'Notes',
+    dataIndex: 'notes',
+    key: 'notes',
+    render: () => <EditTwoTone />,
+    align: 'center',
+  },
+  {
+    title: 'Quantity',
+    dataIndex: 'quantity',
+    key: 'noquantitytes',
+    align: 'center',
+  },
+  {
+    title: 'Recieved Location',
+    dataIndex: 'recieved_location',
+    key: 'recieved_location',
+  },
+];
 interface ITransferManagement {
   changeManagementTab: (tabName: string) => void;
 }
@@ -11,110 +142,7 @@ const TransferManagement: React.FC<ITransferManagement> = ({ changeManagementTab
   const [dataSource, setDataSource] = useState(data);
   const [historyDataSource, sethistoryDataSource] = useState(historyData);
   const [recieveDataSource, setrecieveDataSource] = useState(recieveData);
-
-  const Tcolumns = [
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-    },
-    {
-      title: 'Source Warehouse',
-      dataIndex: 'source_warehouse',
-      key: 'source_warehouse',
-    },
-    {
-      title: 'Order Number',
-      dataIndex: 'order_number',
-      key: 'order_number',
-    },
-    {
-      title: 'FBA Shipment',
-      dataIndex: 'fba_shipment',
-      key: 'fba_shipment',
-    },
-    {
-      title: 'Create Date',
-      dataIndex: 'created_date',
-      key: 'created_date',
-    },
-    {
-      title: 'Closed Date',
-      dataIndex: 'closed_date',
-      key: 'closed_date',
-    },
-    {
-      title: 'Notes',
-      dataIndex: 'notes',
-      key: 'notes',
-    },
-    {
-      title: 'Receiving Warehouse',
-      dataIndex: 'receiving_warehouse',
-      key: 'receiving_warehouse',
-    },
-    {
-      title: 'Total',
-      dataIndex: 'total',
-      key: 'total',
-    },
-    {
-      title: 'Shipping',
-      dataIndex: 'shipping',
-      key: 'shipping',
-    },
-    {
-      title: 'Tracking',
-      dataIndex: 'tracking',
-      key: 'tracking',
-    },
-  ];
-
-  const Hcolumns = [
-    {
-      title: 'Edit Time',
-      dataIndex: 'edit_time',
-      key: 'edit_time',
-    },
-    {
-      title: 'User',
-      dataIndex: 'user',
-      key: 'user',
-    },
-    {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-    },
-  ];
-
-  const Rcolumns = [
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-    },
-    {
-      title: 'Master SKU',
-      dataIndex: 'master_sku',
-      key: 'master_sku',
-    },
-    {
-      title: 'Notes',
-      dataIndex: 'notes',
-      key: 'notes',
-    },
-    {
-      title: 'Quantity',
-      dataIndex: 'quantity',
-      key: 'noquantitytes',
-    },
-    {
-      title: 'Recieved Location',
-      dataIndex: 'recieved_location',
-      key: 'recieved_location',
-    },
-  ];
+  const [selectedTranster, setSelectedTranster] = useState(null);
 
   const [openHistory, setopenHistory] = useState(false);
   const [openReceive, setopenReceive] = useState(false);
@@ -133,8 +161,8 @@ const TransferManagement: React.FC<ITransferManagement> = ({ changeManagementTab
 
   return (
     <>
-      <Row>
-        <Col span={24}>
+      <Row gutter={20} style={{ width: '100%' }}>
+        <Col span={16}>
           <Row justify="space-between">
             <Col span={12} style={{ paddingLeft: 10 }}>
               <Button
@@ -148,25 +176,79 @@ const TransferManagement: React.FC<ITransferManagement> = ({ changeManagementTab
                 TRANSFERS
               </Button>
             </Col>
+            <Col span={12}>
+              <Row style={{ marginBottom: '10px' }} gutter={10}>
+                <Col>
+                  <Checkbox value="external_fba">External FBA</Checkbox>
+                </Col>
+                <Col>
+                  <Select
+                    options={[{ value: 'source_warehouse', label: '37 source Warehouses' }]}
+                    defaultValue="source_warehouse"
+                    size="small"
+                    style={{ width: '200px' }}
+                  />
+                </Col>
+                <Col>
+                  <Select
+                    options={[{ value: 'dest_warehouse', label: '37 Dest. Warehouses' }]}
+                    defaultValue="dest_warehouse"
+                    size="small"
+                    style={{ width: '100px' }}
+                  />
+                </Col>
+                <Col>
+                  <Select
+                    options={[{ value: 'sto_statuses', label: '7 STO Statuses' }]}
+                    defaultValue="sto_statuses"
+                    size="small"
+                    style={{ width: '100px' }}
+                  />
+                </Col>
+                <Col>
+                  <Select
+                    options={[{ value: 'fba_statuses', label: '9 FBA Statuses' }]}
+                    defaultValue="fba_statuses"
+                    size="small"
+                    style={{ width: '100px' }}
+                  />
+                </Col>
+              </Row>
+            </Col>
           </Row>
-        </Col>
-        <Col span={16}>
-          <Card style={{ marginTop: 10 }}>
-            <div style={{ width: 300, display: 'inline-block', marginRight: '10px' }}>
-              <Search
-                placeholder="Enter Order#, SKU or product name..."
-                onSearch={() => console.log('Inactive')}
-                enterButton
-              />
-            </div>
-            <Button type="primary" onClick={showReceive} style={{ marginRight: '10px' }}>
-              Receive
-            </Button>
-            <Button type="primary" onClick={showHistory} style={{ marginRight: '10px' }}>
-              History
-            </Button>
+          <Card style={{ marginTop: 10, borderRadius: 10, marginLeft: 10, width: '100%' }}>
+            <Row gutter={10} align="middle">
+              <Col>
+                <div style={{ width: 300, display: 'inline-block' }}>
+                  <Search
+                    placeholder="Enter Order#, SKU or product name..."
+                    onSearch={() => console.log('Inactive')}
+                    enterButton
+                    size="small"
+                  />
+                </div>
+              </Col>
+              <Col>
+                <Button
+                  type="primary"
+                  onClick={showReceive}
+                  disabled={!(selectedTranster?.status === 'pending_receiving')}
+                >
+                  Receive
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  type="primary"
+                  onClick={showHistory}
+                  disabled={!(selectedTranster?.status === 'pending_receiving')}
+                >
+                  History
+                </Button>
+              </Col>
+            </Row>
             <Drawer
-              title="History For Stock Transfer"
+              title="History For Stock Transfer Order "
               width={800}
               placement="right"
               onClose={closeHistory}
@@ -174,9 +256,25 @@ const TransferManagement: React.FC<ITransferManagement> = ({ changeManagementTab
             >
               <OTable columns={Hcolumns} rows={historyDataSource} />
             </Drawer>
-          </Card>
-          <Card>
-            <OTable columns={Tcolumns} rows={dataSource} />
+            <Table
+              columns={Tcolumns}
+              dataSource={dataSource}
+              style={{ marginTop: 15 }}
+              onRow={(record) => {
+                return {
+                  onClick: () => {
+                    if (record.key === selectedTranster?.key) setSelectedTranster(null);
+                    else setSelectedTranster(record);
+                  }, // click row
+                };
+              }}
+              rowSelection={{
+                selectedRowKeys: selectedTranster ? [selectedTranster.key] : [],
+                hideSelectAll: true,
+                columnWidth: 0, // Set the width to 0
+                renderCell: () => '', // Render nothing inside
+              }}
+            />
           </Card>
         </Col>
         {openReceive && (
