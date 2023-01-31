2cc5e0665432aa6a96a7ff0a58b842fe1956b0ff
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-01-31 10:40:50 +0900

    refactor(SKUB-20): refeactor the download excel template link in import product modal

--- src/components/Modals/Product/ImportProducts.tsx
+++ src/components/Modals/Product/ImportProducts.tsx
@@ -2,37 +2,36 @@ import { OModal } from '@/components/Globals/OModal';
 import { Button, Col, Row, Select, Upload } from 'antd';
 import { fileUploadProps } from '@/utils/helpers/base';
 import { UploadOutlined } from '@ant-design/icons';
-import httpClient from '@/utils/http-client';
 const { Option } = Select;
 
-interface IImportProducts {
+interface IImportProduct {
   isOpen: boolean;
   onClose: () => void;
   onSave: () => void;
 }
 
-const ImportProducts: React.FC<IImportProducts> = ({ isOpen, onClose, onSave }) => {
+const ImportProduct: React.FC<IImportProduct> = ({ isOpen, onClose, onSave }) => {
   const handleSave = () => onSave();
 
-  const handleDownloadTemplate = () => {
-    httpClient
-      .post('/api/products/import_template', {}, { responseType: 'arraybuffer' })
-      .then((res) => {
-        const fileName = 'Excel Template for Product Import.xlsx';
-        const url = window.URL.createObjectURL(
-          new Blob([res.data], {
-            type: 'application/vnd.ms-excel',
-          }),
-        );
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', fileName);
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-      })
-      .catch((error) => console.log(error));
-  };
+  // const handleDownloadTemplate = () => {
+  //   httpClient
+  //     .post('/api/products/import_template', {}, { responseType: 'arraybuffer' })
+  //     .then((res) => {
+  //       const fileName = 'Excel Template for Product Import.xlsx';
+  //       const url = window.URL.createObjectURL(
+  //         new Blob([res.data], {
+  //           type: 'application/vnd.ms-excel',
+  //         }),
+  //       );
+  //       const link = document.createElement('a');
+  //       link.href = url;
+  //       link.setAttribute('download', fileName);
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       link.remove();
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
 
   return (
     <OModal
@@ -56,16 +55,21 @@ const ImportProducts: React.FC<IImportProducts> = ({ isOpen, onClose, onSave }) 
       ]}
     >
       <>
-        <p>All batch imports into Skubana are done through the Microsoft Excel spreadsheet format.</p>
+        <p>
+          All batch imports into Skubana are done through the Microsoft Excel spreadsheet format.
+        </p>
         <a
-          onClick={handleDownloadTemplate}
           className="download-link"
+          href={`${BACKEND_URL || 'http://127.0.0.1:8000'}/template/product_import_template.xlsx`}
         >
           <u>Download the Excel Template for Product Import</u>
         </a>
         <p>
-          Every product is uniquely identified by its <b>Internal SKU</b>, and those SKU's are 
-          <i><b> not</b></i> case sensitive. For example, <i>'sku123' </i>
+          Every product is uniquely identified by its <b>Internal SKU</b>, and those SKU's are
+          <i>
+            <strong> not</strong>
+          </i>{' '}
+          case sensitive. For example, <i>'sku123' </i>
           is regarded the same as <i>'SKU123'</i> by the system.
         </p>
         <p>
@@ -75,15 +79,18 @@ const ImportProducts: React.FC<IImportProducts> = ({ isOpen, onClose, onSave }) 
           to correct duplicate values and re-submit.
         </p>
         <Row>
-          <Col>
+          <Col offset={16} span={8} style={{ textAlign: 'right' }}>
             <label>Products File:</label>&nbsp;&nbsp;
             <Upload {...fileUploadProps}>
               <Button icon={<UploadOutlined />}>Select...</Button>
             </Upload>
           </Col>
-          <Col style={{ marginTop: '1rem' }}>
+          <Col span={24} style={{ textAlign: 'right', marginTop: '1rem' }}>
             <label>Update existing products if changes found in the Excel file?</label>&nbsp;&nbsp;
-            <Select placeholder="Yes - Update existing products and import new" style={{ textIndent: '0.5rem' }}>
+            <Select
+              placeholder="Yes - Update existing products and import new"
+              style={{ textIndent: '0.5rem' }}
+            >
               <Option value="1">Yes - Update existing products and import new.</Option>
               <Option value="2">No - Ignore exisiting products; only import new.</Option>
             </Select>
@@ -94,4 +101,4 @@ const ImportProducts: React.FC<IImportProducts> = ({ isOpen, onClose, onSave }) 
   );
 };
 
-export default ImportProducts;
+export default ImportProduct;
