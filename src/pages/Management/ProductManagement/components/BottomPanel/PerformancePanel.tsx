699134060c4c00ae13ef121ccfb5b9b6fba69b61
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-02-08 21:29:53 +0900

    build(SKUB-47): build year over year graph in the product performance panel

--- src/pages/Management/ProductManagement/components/BottomPanel/PerformancePanel.tsx
+++ src/pages/Management/ProductManagement/components/BottomPanel/PerformancePanel.tsx
@@ -1,8 +1,28 @@
 import { OButton } from '@/components/Globals/OButton';
 import { OInput } from '@/components/Globals/OInput';
-import { Card, Space, Form } from 'antd';
+import { Line } from '@ant-design/charts';
+import { MenuOutlined } from '@ant-design/icons';
+import { Card, Space, Form, Dropdown, Row, Col } from 'antd';
+import { useRef, useState } from 'react';
+import { useReactToPrint } from 'react-to-print';
+
+const yoyChartData = [];
+
+for (const d = new Date(2021, 10, 7); d <= new Date(2022, 0, 5); d.setDate(d.getDate() + 1)) {
+  yoyChartData.push({
+    date: `${d.getMonth()}/${d.getDate()}`,
+    value: 0,
+  });
+}
 
 const PerformancePanel: React.FC = () => {
+  const [yoyChartInstance, setYOYChartInstance] = useState(null);
+  const yoyChartRef = useRef(null);
+
+  const handleYOYChartPrint = useReactToPrint({
+    content: () => yoyChartRef.current,
+  });
+
   return (
     <Card
       title="Performance"
@@ -25,7 +45,7 @@ const PerformancePanel: React.FC = () => {
                 text: '30 Days',
               },
             ]}
-            onChange={() => {}}
+            style={{ width: 200 }}
           />
         </Form.Item>
         <Form.Item>
@@ -39,12 +59,85 @@ const PerformancePanel: React.FC = () => {
                 text: 'Quantity Solds',
               },
             ]}
-            onChange={() => {}}
+            style={{ width: 200 }}
           />
         </Form.Item>
       </Form>
-      <div style={{ textAlign: 'center', padding: '1rem' }}>
-        Select a product to view performance
+      <Row justify="end">
+        <Col>
+          <Dropdown.Button
+            menu={{
+              items: [
+                {
+                  key: 'print_chart',
+                  label: 'Print Chart',
+                  disabled: !yoyChartInstance,
+                  onClick: () => handleYOYChartPrint(),
+                },
+                {
+                  type: 'divider',
+                },
+                {
+                  key: 'download_png',
+                  label: 'Download PNG image',
+                  disabled: !yoyChartInstance,
+                  onClick: () =>
+                    yoyChartInstance.downloadImage('Year-Over-Year-chart', 'image/png'),
+                },
+                {
+                  key: 'download_jpeg',
+                  label: 'Download JPEG image',
+                  disabled: !yoyChartInstance,
+                  onClick: () =>
+                    yoyChartInstance.downloadImage('Year-Over-Year-chart', 'image/jpeg'),
+                },
+                {
+                  key: 'download_pdf',
+                  label: 'Download PDF document',
+                  disabled: true,
+                },
+                {
+                  key: 'download_svg',
+                  label: 'Download SVG Vector image',
+                  disabled: true,
+                },
+              ],
+            }}
+            placement="bottomRight"
+            icon={<MenuOutlined />}
+          />
+        </Col>
+      </Row>
+      <div ref={yoyChartRef}>
+        <Line
+          data={yoyChartData}
+          xField="date"
+          yField="value"
+          yAxis={{
+            title: {
+              text: 'Quantity',
+              position: 'center',
+            },
+            min: -1,
+            tickInterval: 1,
+          }}
+          interactions={[
+            {
+              type: 'element-selected',
+            },
+            {
+              type: 'element-active',
+            },
+          ]}
+          legend={{
+            position: 'bottom',
+          }}
+          point={{
+            shape: 'circle',
+          }}
+          height={100}
+          onReady={(chartInstance) => setYOYChartInstance(chartInstance)}
+        />
       </div>
     </Card>
   );
