3816a3df708613c5be62c4aecd9617a81292f1dc
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-02-13 13:33:33 +0900

    refactor(SKUB-53): refactor filter by panel in the products module

--- src/pages/Management/ProductManagement/components/LeftPanel/searchByCategoriesPanel.tsx
+++ src/pages/Management/ProductManagement/components/LeftPanel/searchByCategoriesPanel.tsx
@@ -1,7 +1,5 @@
 import BookIcon from '@/utils/icons/book';
-import {
-  CaretDownOutlined,
-} from '@ant-design/icons';
+import { CaretDownOutlined } from '@ant-design/icons';
 import { Tree } from 'antd';
 import type { DataNode } from 'antd/es/tree';
 import React, { useState } from 'react';
@@ -97,37 +95,40 @@ const SearchByCategoriesPanel: React.FC = () => {
 
   const onSelect = (selectedKeysValue: React.Key[], event: any) => {
     if (event.nativeEvent.ctrlKey) {
-      setSelectedKeys(selectedKeysValue)
+      setSelectedKeys(selectedKeysValue);
     } else {
-      setSelectedKeys([event.node.key]);
+      if (selectedKeys[0] === event.node.key) setSelectedKeys([]);
+      else setSelectedKeys([event.node.key]);
     }
   };
 
-  return <div>
-    <h3>Hold 'Ctrl' or '' Key to select multiple.</h3>
-    <Tree
-      showIcon
-      defaultExpandAll
-      defaultSelectedKeys={['0-0-0']}
-      switcherIcon={<CaretDownOutlined />}
-      treeData={treeCategoryData}
-      rootStyle={{ fontSize: 15 }}
-      selectedKeys={selectedKeys}
-      onSelect={onSelect}
-      multiple
-    />
-    <Tree
-      showIcon
-      defaultExpandAll
-      defaultSelectedKeys={['0-0-0']}
-      switcherIcon={<CaretDownOutlined />}
-      treeData={treeLabelData}
-      rootStyle={{ fontSize: 15 }}
-      selectedKeys={selectedKeys}
-      onSelect={onSelect}
-      multiple
-    />
-  </div>
+  return (
+    <div>
+      <h3>Hold 'Ctrl' or '' Key to select multiple.</h3>
+      <Tree
+        showIcon
+        defaultExpandAll
+        defaultSelectedKeys={['0-0-0']}
+        switcherIcon={<CaretDownOutlined />}
+        treeData={treeCategoryData}
+        rootStyle={{ fontSize: 15 }}
+        selectedKeys={selectedKeys}
+        onSelect={onSelect}
+        multiple
+      />
+      <Tree
+        showIcon
+        defaultExpandAll
+        defaultSelectedKeys={['0-0-0']}
+        switcherIcon={<CaretDownOutlined />}
+        treeData={treeLabelData}
+        rootStyle={{ fontSize: 15 }}
+        selectedKeys={selectedKeys}
+        onSelect={onSelect}
+        multiple
+      />
+    </div>
+  );
 };
 
 export default SearchByCategoriesPanel;
