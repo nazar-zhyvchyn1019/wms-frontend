38ff67d7df69d43e42acae1582e8f68291b367e0
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-02-15 14:55:32 +0900

    refactor(SKUB-51): refactor bottom panel in the purchase module

--- src/pages/Management/PurchaseOrderManagement/components/Bottoms/tabcomponent.tsx
+++ src/pages/Management/PurchaseOrderManagement/components/Bottoms/tabcomponent.tsx
@@ -1,38 +1,65 @@
+import { useState } from 'react';
+import type { FC } from 'react';
 import PODetailsBottom from '@/components/PurchaseOrder/Details';
 import ItemsManagement from '@/components/PurchaseOrder/Items';
-import { Tabs } from 'antd';
-import type { FC } from 'react';
+import { Radio, Card, Space } from 'antd';
+import type { RadioChangeEvent } from 'antd';
+import { useModel } from '@umijs/max';
 
 interface ITabComponent {
   POProductItems: any[];
 }
 
 const TabComponent: FC<ITabComponent> = ({ POProductItems }) => {
-  const tabListNoTitle = [
-    {
-      key: 'items',
-      label: 'Items',
-      children: <ItemsManagement data={POProductItems} />,
-    },
-    {
-      key: 'details',
-      label: 'Details',
-      children: <PODetailsBottom />,
-    },
-    {
-      key: 'history',
-      label: 'History',
-      children: <p>project content</p>,
-    },
-  ];
+  const [selectedMode, setSelectedMode] = useState('items');
+  const { selectedPO } = useModel('po');
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setSelectedMode(e.target.value);
+  };
 
   return (
-    <Tabs
-      defaultActiveKey="items"
-      items={tabListNoTitle}
-      tabBarExtraContent={<a href="#">P.O. #FORKS1114</a>}
-      style={{ paddingLeft: 10, paddingRight: 10 }}
-    />
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginTop: 5,
+          marginLeft: 10,
+          marginRight: 10,
+        }}
+      >
+        <h2>{selectedPO?.ponumber}</h2>
+        <Radio.Group
+          style={{ marginBottom: 8 }}
+          size="large"
+          value={selectedMode}
+          onChange={handleChange}
+        >
+          <Space size={5}>
+            <Radio.Button value="items">Items</Radio.Button>
+            <Radio.Button value="details">Details</Radio.Button>
+            <Radio.Button value="history">History</Radio.Button>
+          </Space>
+        </Radio.Group>
+      </div>
+      <Card>
+        {selectedMode === 'items' ? (
+          <ItemsManagement data={POProductItems} />
+        ) : selectedMode === 'details' ? (
+          <PODetailsBottom />
+        ) : (
+          <p>project content</p>
+        )}
+      </Card>
+      {/* <Tabs
+        defaultActiveKey="items"
+        items={tabListNoTitle}
+        tabBarExtraContent={<a href="#">P.O. #FORKS1114</a>}
+        style={{ paddingLeft: 10, paddingRight: 10 }}
+      /> */}
+    </>
   );
 };
 
