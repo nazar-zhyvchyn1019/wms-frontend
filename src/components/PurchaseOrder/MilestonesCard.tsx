f178bc00b3a53d6f1ef27968ab951c9925bdd5f0
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-02-18 01:09:57 +0900

    refactor(SKUB-47): refactor milestone card in section in the purchase module

--- src/components/PurchaseOrder/MilestonesCard.tsx
+++ src/components/PurchaseOrder/MilestonesCard.tsx
@@ -1,36 +1,44 @@
-import React, {useEffect} from 'react';
-import {  
-  Card,
-  Row,
-  Col,
-  Checkbox,
-} from 'antd';
+import React, { useState } from 'react';
+import { Card, Button } from 'antd';
+import { useModel } from '@umijs/max';
+import { CheckSquareFilled } from '@ant-design/icons';
+import { OTable } from '../Globals/OTable';
+import ConfigureMilestonesModal from '../Modals/PurchaseOrder/ConfigureMilestonesModal';
 
-interface IMilestonesCard {
-    data?: any;
-}
+const TColumns = [
+  {
+    id: 'text',
+    dataIndex: 'id',
+    key: 'id',
+    title: 'Milestone',
+    render: (id, milestone) => (
+      <div style={{ display: 'flex', gap: 10 }}>
+        <CheckSquareFilled style={{ color: milestone.color, fontSize: 15 }} />
+        {milestone.text}
+      </div>
+    ),
+  },
+];
 
-const MilestonesCard: React.FC<IMilestonesCard> = ({
-    data,
-}) => {
+const MilestonesCard: React.FC = () => {
+  const { milestonesList } = useModel('milestones');
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => { console.log(data)})
+  return (
+    <>
+      <Card title="Milestones">
+        <div style={{ marginTop: 10 }}>
+          <OTable columns={TColumns} rows={milestonesList} pagination={false} />
+        </div>
 
-    return (
-        <Card title="Milestones">
-            <Row>
-                <Col span={2}>
-                    <Checkbox checked/>
-                </Col>
-                <Col span={22}>
-                    30% Deposit Paid
-                </Col>
-            </Row>
-            <Row>
-                <Card style={{ height: '100%', width: '100%', marginTop: '10%', padding: '10%' }}></Card>
-            </Row>
-        </Card>
-    );
+        <Button style={{ marginTop: 10 }} onClick={() => setIsOpen(true)}>
+          Manage Milestone
+        </Button>
+      </Card>
+
+      <ConfigureMilestonesModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
+  );
 };
 
 export default MilestonesCard;
