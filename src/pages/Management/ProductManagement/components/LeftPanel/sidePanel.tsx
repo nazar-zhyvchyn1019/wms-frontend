8f7b7f6b13dd965a848b779df0314cebbca38f67
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2023-02-17 21:32:54 +0900

    refactor(SKUB-47): refactor left panel in the product module

--- src/pages/Management/ProductManagement/components/LeftPanel/sidePanel.tsx
+++ src/pages/Management/ProductManagement/components/LeftPanel/sidePanel.tsx
@@ -1,39 +1,35 @@
-import React, { useState } from 'react';
-import { Button, Row, Col } from 'antd';
-import { DoubleRightOutlined, SearchOutlined } from '@ant-design/icons';
+import { FilterOutlined, SearchOutlined } from '@ant-design/icons';
+import { Tabs } from 'antd';
 import SearchByProductPanel from './searchByProductPanel';
 import SearchByCategoriesPanel from './searchByCategoriesPanel';
 
 const SidePanel: React.FC = () => {
-  const [searchType, setSearchType] = useState(true);
+  const items = [
+    {
+      label: (
+        <span>
+          <FilterOutlined />
+          Filter By
+        </span>
+      ),
+      key: '1',
+      children: <SearchByCategoriesPanel />,
+    },
+    {
+      label: (
+        <span>
+          <SearchOutlined />
+          Search By
+        </span>
+      ),
+      key: '2',
+      children: <SearchByProductPanel />,
+    },
+  ];
 
   return (
-    <div className='left-panel'>
-      <Row justify="space-between" align="middle">
-        <Col flex="auto">
-          <div style={{ marginLeft: '5px' }}>
-            <h2>{searchType ? 'Search Products...' : 'Filter By'}</h2>
-          </div>
-        </Col>
-        <Col>
-          <Button
-            icon={
-              searchType ? (
-                <DoubleRightOutlined rotate={270} />
-              ) : (
-                <DoubleRightOutlined rotate={90} />
-              )
-            }
-            onClick={() => setSearchType(!searchType)}
-            size="large"
-            ghost
-            style={{ border: 'none' }}
-          >
-            <SearchOutlined />
-          </Button>
-        </Col>
-      </Row>
-      <div>{searchType ? <SearchByProductPanel /> : <SearchByCategoriesPanel />}</div>
+    <div className="left-panel" style={{ padding: 2 }}>
+      <Tabs size="small" defaultActiveKey="1" items={items} />
     </div>
   );
 };
